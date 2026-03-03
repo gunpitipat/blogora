529c738a72e280b61153f64316e2003cdc328c61
gunpitipat <gunpitipat@outlook.com>
2026-03-03 14:22:39 +0700

    refactor(repo): archive old frontend as frontend-legacy and initialize new frontend app

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,41 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from "eslint-plugin-react"
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      react,
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react/jsx-uses-vars': 'error',
-      'react-refresh/only-export-components': 'off'
     },
   },
-  {
-    files: ['vite.config.js'],
-    languageOptions: {
-      globals: globals.node
-    }
-  }
-])
+]);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <>Hello Blogora</>;
+};
+
+export default App;
